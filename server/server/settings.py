4408530248cb691a4b20f8c07b363b48046f877f
Jorge Isaza <isaza.jorge@hotmail.com>
2022-08-13 14:57:32 -0500

    chore(server): cors and updated requirements.txt

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,6 +25,9 @@ ALLOWED_HOSTS = [
     config('ALLOWED_HOST'),
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    config('ALLOWED_ORIGIN'),
+]
 
 # Application definition
 
@@ -36,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
